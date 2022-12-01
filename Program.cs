19de03906cec705f12592919f4bc342cc6b866ca
Richard Goforth <dedac@github.com>
2022-12-01 15:34:57 -0500

    cleaning

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿var lines = File.ReadAllLines(args[0]).AsEnumerable();
+﻿var lines = File.ReadLines(args[0]);
 var elves = lines.SplitBy((s) => s == "");
 var elvesByCals = elves.Select(elf => elf.Select((s) => int.Parse(s)).Sum());
 Console.WriteLine(elvesByCals.OrderByDescending(i => i).Take(3).Sum());
---
+++ day1/Day1.cs
@@ -0,0 +1,17 @@
+﻿internal class Days
+{
+    private static void Day1(string[] args)
+    {
+        // No helper function
+        // var elves = File.ReadAllText(args[0]).Split("\n\n");
+        // var elvesByCals = elves.Select(
+        //     elf => elf.Split('\n', StringSplitOptions.RemoveEmptyEntries)
+        //     .Select((s) => int.Parse(s)).Sum());
+        // Console.WriteLine(elvesByCals.OrderByDescending(i => i).Take(3).Sum());
+
+        var lines = File.ReadLines(args[0]);
+        var elves = lines.SplitBy((s) => s == "");
+        var elvesByCals = elves.Select(elf => elf.Select((s) => int.Parse(s)).Sum());
+        Console.WriteLine(elvesByCals.OrderByDescending(i => i).Take(3).Sum());
+    }
+}
