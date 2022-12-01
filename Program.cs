5704885499ef0a5f1d3390a208a7cb9703783797
Richard Goforth <dedac@github.com>
2022-12-01 13:55:46 -0500

    Day 1 complete

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿var lines = File.ReadAllLines(args[0]).AsEnumerable();
-var elves = lines.SplitBy((s) => s != "");
-var maxCals = elves.Max(elf => elf.Select((s) => int.Parse(s)).Sum());
-
-Console.WriteLine(maxCals);
+var elves = lines.SplitBy((s) => s == "");
+var elvesByCals = elves.Select(elf => elf.Select((s) => int.Parse(s)).Sum());
+Console.WriteLine(elvesByCals.OrderByDescending(i => i).Take(3).Sum());
