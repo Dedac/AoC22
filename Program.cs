b67edffe55c18325aa3a187b17f612bbc19c6bca
Richard Goforth <dedac@github.com>
2022-12-02 01:55:47 -0500

    day 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
 ﻿var lines = File.ReadLines(args[0]);
-var elves = lines.SplitBy((s) => s == "");
-var elvesByCals = elves.Select(elf => elf.Select((s) => int.Parse(s)).Sum());
-Console.WriteLine(elvesByCals.OrderByDescending(i => i).Take(3).Sum());
+var sum = 0;
+foreach (var line in lines)
+{
+    var g = line.Split(" ");
+    if (g[1] == "X") {
+        if (g[0] == "A") sum += 3;
+        else if (g[0] == "B") sum += 1;
+        else sum += 2; //C
+    }
+    else if (g[1] == "Y") { 
+        sum += 3 + g[0].First() - 64; // Yay char math!
+    }
+    else { //"Z"
+        if (g[0] == "A") sum += 2;
+        else if (g[0] == "B") sum += 3;
+        else sum += 1; //C
+        sum += 6;
+    }
+}
+Console.WriteLine(sum);
