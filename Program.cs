dbaf6659c8f8cf20a40c7aa90fa443a2ce555099
Richard Goforth <dedac@github.com>
2022-12-06 00:37:48 -0500

    Day6

--- Program.cs
+++ Program.cs
@@ -1,19 +1,4 @@
-﻿var boxRows = File.ReadLines(args[0])
-                .TakeWhile(s => !s.StartsWith(" "))
-                .Select(s => s.Chunk(4).Select(s => s[1]).ToArray());
-
-var stacks = new string[boxRows.Last().Length];
-for (int i = 0; i < boxRows.Last().Length; i++)
-    stacks[i] = (new string(boxRows.Select(b => b[i]).ToArray())).Trim();
-
-File.ReadLines(args[0]).SkipWhile(s => !s.StartsWith("move"))
-        .Select(s => s.Split(' ').Chunk(2).Select(w => int.Parse(w[1]) - 1).ToArray())
-        .All(m =>
-        {
-            var boxes = stacks[m[1]].Substring(0, m[0] + 1);
-            stacks[m[1]] = stacks[m[1]].Substring(m[0] + 1);
-            stacks[m[2]] = boxes + stacks[m[2]];
-            return true;
-        });
-
-Console.WriteLine(string.Join('\n', stacks));
+﻿var signal = File.ReadAllText(args[0]);
+for (int i = 0; i<signal.Length;i++)
+  if (signal.Skip(i).Take(14).Distinct().Count() == 14) 
+  Console.WriteLine(i+14);
---
+++ day6/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Day6
+{
+    private static void Day(string[] args)
+    {
+        var signal = File.ReadAllText(args[0]);
+        for (int i = 0; i < signal.Length; i++)
+            if (signal.Skip(i).Take(14).Distinct().Count() == 14)
+                Console.WriteLine(i + 14);
+    }
+}
