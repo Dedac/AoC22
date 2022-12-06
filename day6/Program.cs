6ab91226ceae852053e6c3f285e254b57a1b53bb
Richard Goforth <dedac@github.com>
2022-12-06 01:40:06 -0500

    Once through the file

--- day6/Program.cs
+++ day6/Program.cs
@@ -2,16 +2,17 @@
 {
     private static void Day(string[] args)
     {
-        var signal = File.ReadAllText(args[0]);
-
-        var getIndex = (int size) =>
+        var getIndex = (int size, StreamReader signal) =>
         {
-            for (int i = size; i < signal.Length; i++)
-                if (signal[(i - size)..i].Distinct().Count() == size)
-                    return i;
-            return -1;
+            var buff = new char[size];
+            for (int i = 0; true; i++)
+            {
+                buff[i % size] = (char)signal.Read();
+                if (i > size && buff.Distinct().Count() == size)
+                    return i + 1;
+            }
         };
-        Console.WriteLine(getIndex(4));
-        Console.WriteLine(getIndex(14));
+        Console.WriteLine(getIndex(4, File.OpenText(args[0])));
+        Console.WriteLine(getIndex(14, File.OpenText(args[0])));
     }
 }
