55dbb1c40de7101daf52cb31ff8d719a0fff4c4a
Richard Goforth <dedac@github.com>
2022-12-09 09:55:06 -0500

    Readability brush for fun

--- Program.cs
+++ Program.cs
@@ -1,20 +1,19 @@
-﻿var treeMatrix = File.ReadLines(args[0]) //read the trees into an int 'matrix'
-    .Select(l => l.Select(c => c - '0').ToArray()).ToArray();
-int visibleTrees = 0;
-int maxScenicScore = 0;
-for (int i = 0; i < treeMatrix.Length; i++) //rows
-    for (int j = 0; j < treeMatrix[i].Length; j++) //columns
-    {   //look out from this tree
-        var viewsFromTree = new List<IEnumerable<int>>() {
-                            treeMatrix[..i].Select(a => a[j]).Reverse(), //up
-                            treeMatrix[(i + 1)..].Select(a => a[j]), //down
-                            treeMatrix[i][..j].Reverse(), //left
-                            treeMatrix[i][(j + 1)..] }; //right
-        // Check each direction to see if there are not any same or taller trees
-        if (viewsFromTree.Any(v => !v.Any(t => t >= treeMatrix[i][j]))) visibleTrees++;
-        // Multiply views from each direction, and select the max
-        maxScenicScore = Math.Max(maxScenicScore, 
-            viewsFromTree.Aggregate(1, (c, v) => c * v.TakeUntil(t => t < treeMatrix[i][j]).Count()));
-    }
-Console.WriteLine(visibleTrees);
-Console.WriteLine(maxScenicScore);
+﻿/* You are a landscape architect. You are designing a new park. You are given a matrix of numbers representing trees of different sizes. You are asked to find the number of trees that can be seen from any location in the park. Trees can be seen from any location if there are no same or taller trees between them. Also, find the maximum scenic score of the park. The scenic score of the park is the product of the number of trees visible from each location. */
+        var treeMatrix = File.ReadLines(args[0])
+            .Select(l => l.Select(c => c - '0').ToArray()).ToArray();
+        int visibleTrees = 0;
+        int maxScenicScore = 0;
+        for (int i = 0; i < treeMatrix.Length; i++)
+            for (int j = 0; j < treeMatrix[i].Length; j++)
+            {
+                var viewsFromTree = new List<IEnumerable<int>>() {
+                                    treeMatrix[..i].Select(a => a[j]).Reverse(),
+                                    treeMatrix[(i + 1)..].Select(a => a[j]),
+                                    treeMatrix[i][..j].Reverse(),
+                                    treeMatrix[i][(j + 1)..] };
+                if (viewsFromTree.Any(v => !v.Any(t => t >= treeMatrix[i][j]))) visibleTrees++;
+                maxScenicScore = Math.Max(maxScenicScore, 
+                    viewsFromTree.Aggregate(1, (c, v) => c * v.TakeUntil(t => t < treeMatrix[i][j]).Count()));
+            }
+        Console.WriteLine(visibleTrees);
+        Console.WriteLine(maxScenicScore);
