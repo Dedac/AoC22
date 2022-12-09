a19459dcdd8e33a317c59abf1a021cb36fc0d4d6
Richard Goforth <dedac@github.com>
2022-12-09 17:06:57 -0500

    day9 pt1

--- Program.cs
+++ Program.cs
@@ -1,19 +1,28 @@
-﻿/* You are a landscape architect. You are designing a new park. You are given a matrix of numbers representing trees of different sizes. You are asked to find the number of trees that can be seen from any location in the park. Trees can be seen from any location if there are no same or taller trees between them. Also, find the maximum scenic score of the park. The scenic score of the park is the product of the number of trees visible from each location. */
-        var treeMatrix = File.ReadLines(args[0])
-            .Select(l => l.Select(c => c - '0').ToArray()).ToArray();
-        int visibleTrees = 0;
-        int maxScenicScore = 0;
-        for (int i = 0; i < treeMatrix.Length; i++)
-            for (int j = 0; j < treeMatrix[i].Length; j++)
-            {
-                var viewsFromTree = new List<IEnumerable<int>>() {
-                                    treeMatrix[..i].Select(a => a[j]).Reverse(),
-                                    treeMatrix[(i + 1)..].Select(a => a[j]),
-                                    treeMatrix[i][..j].Reverse(),
-                                    treeMatrix[i][(j + 1)..] };
-                if (viewsFromTree.Any(v => !v.Any(t => t >= treeMatrix[i][j]))) visibleTrees++;
-                maxScenicScore = Math.Max(maxScenicScore, 
-                    viewsFromTree.Aggregate(1, (c, v) => c * v.TakeUntil(t => t < treeMatrix[i][j]).Count()));
-            }
-        Console.WriteLine(visibleTrees);
-        Console.WriteLine(maxScenicScore);
+﻿var H = (0, 0);
+var T = (0,0);
+var T2 = new (int,int)[9];
+var visits = new List<(int, int)> { (0, 0) };
+var move = new Dictionary<string, Func<(int, int), (int, int)>>(){
+    {"R", (t) => (t.Item1 + 1, t.Item2)},
+    {"L", (t) => (t.Item1 - 1, t.Item2)},
+    {"U", (t) => (t.Item1, t.Item2 + 1)},
+    {"D", (t) => (t.Item1, t.Item2 - 1)}
+};
+
+foreach (var line in File.ReadLines(args[0]).Select(l => l.Split(' ')))
+{
+    for (int i = 0; i < int.Parse(line[1]); i++)
+    {
+        H = move[line[0]](H);
+
+        var d = (H.Item1 - T.Item1, H.Item2 - T.Item2);
+        if (Math.Abs(d.Item1) > 1 || Math.Abs(d.Item2) > 1)
+        {
+            visits.Add(T);
+            if (d.Item1 != 0) T = move[H.Item1 < T.Item1 ? "L" : "R"](T);
+            if (d.Item2 != 0) T = move[H.Item2 > T.Item2 ? "U" : "D"](T);
+        }
+    }
+}
+visits.Add(T);
+Console.WriteLine(visits.Distinct().Count());
