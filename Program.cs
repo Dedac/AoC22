d4da3327a3a8aeba3c28b0ad57877ca63ec6a86b
Richard Goforth <dedac@github.com>
2022-12-14 15:25:01 -0500

    rewrite of day13 with json parse that works

--- Program.cs
+++ Program.cs
@@ -1,85 +1,46 @@
-﻿Packet Build(string s)
+﻿using System.Text.Json;
+
+JsonElement BuildJson(string s) => JsonSerializer.Deserialize<JsonElement>(s);
+
+int Ordered(JsonElement l, JsonElement r)
 {
-    Packet? c = null;
-    var num = "";
-    for (int i = 0; i < s.Length; i++)
+    if (l.ValueKind == JsonValueKind.Number && r.ValueKind == JsonValueKind.Number)
     {
-        if (s[i] == '[')
-        {
-            var np = new Packet() { Parent = c };
-            c = np;
-        }
-        else if (s[i] == ']' || s[i] == ',')
-        {
-            if (num != "")
-            {
-                c?.Packets.Add(new Packet() { Parent = c, Value = int.Parse(num), Position=c?.Packets.Count()});
-                num = "";
-            }
-            if (s[i] == ']' && c?.Parent != null)
-            {
-                c.Parent.Packets.Add(c);
-                c = c?.Parent;
-            }
-        }
-        else
-            num += s[i];
+        if (l.GetInt32() == r.GetInt32()) return 0;
+        return l.GetInt32() < r.GetInt32() ? -1 : 1;
     }
-    return c ?? new Packet();
+    if (l.ValueKind == JsonValueKind.Number)
+        l = BuildJson($"[{l.GetInt32()}]");
+    if (r.ValueKind == JsonValueKind.Number)
+        r = BuildJson($"[{r.GetInt32()}]");
+
+    var lA = l.EnumerateArray();
+    var rA = r.EnumerateArray();
+    for (int i = 0; i < Math.Min(l.GetArrayLength(), r.GetArrayLength()); i++)
+    {
+        lA.MoveNext();
+        rA.MoveNext();
+        var o = Ordered(lA.Current, rA.Current);
+        if (o != 0) return o;
+    }
+    if (l.GetArrayLength() == r.GetArrayLength()) return 0;
+    return l.GetArrayLength() < r.GetArrayLength() ? -1 : 1;
 }
 
-bool Ordered(Packet? l, Packet? r)
-{
-    if (l == null) return true;
-    if (r == null) return false;
-    if (l.Empty && r.Empty) return Ordered(l.Next(), r.Next());
-    if (l.Value > -1 && r.Value > -1)
-    {
-        if (l.Value == r.Value)
-            return Ordered(l.Next(), r.Next());
-        else
-            return l.Value < r.Value;
-    }
-    if (l.Packets.Count > 0 && r.Packets.Count > 0)
-        return Ordered(l.Packets.First(), r.Packets.First());
-    
-    if (r.Value > -1 && l.Packets.Count > 0){ 
-        r.Packets.Add(new Packet(){Parent = r, Value = r.Value});
-        return Ordered(l.Packets.First(), r.Packets.First());
-    }
-    if (l.Value > -1 && r.Packets.Count > 0){
-        l.Packets.Add(new Packet(){Parent = l, Value = l.Value});
-        return Ordered(l.Packets.First(), r.Packets.First());
-    }
-    if (l.Packets.Count == 0 && r.Packets.Count > 0) return true;
-    if (r.Packets.Count == 0 && l.Packets.Count > 0) return false;
-    if (!l.Empty && r.Empty) return false;
-    if (l.Empty && !r.Empty) return true;
+var obj = File.ReadLines(args[0]).SplitBy(s => s == "")
+    .SelectMany(a => a.Select(b => BuildJson(b))).ToList();
 
-    throw new Exception("oops");
-}
+var sumJ = 0;
+for (int i = 0; i < obj.Count / 2; i++)
+    if (Ordered(obj[2 * i], obj[2 * i + 1]) == -1)
+        sumJ += i + 1;
+Console.WriteLine(sumJ);
 
-var packets = File.ReadLines(args[0]).SplitBy(s => s == "")
-    .Select(a => (Build(a.First()), Build(a.Last()))).ToList();
+var distress1 = BuildJson("[[2]]");
+var distress2 = BuildJson("[[6]]");
 
-var sum = 0;
-for (int i = 0; i < packets.Count; i++)
-    if (Ordered(packets[i].Item1, packets[i].Item2))
-        sum += i + 1;
+obj.Add(distress1);
+obj.Add(distress2);
 
-Console.WriteLine(sum);
-
-class Packet
-{
-    public List<Packet> Packets = new List<Packet>();
-    public Packet? Parent = null;
-    public int Value = -1;
-    public int? Position = 0;
-    public Packet? Next()
-    {
-        var i = Position + 1 ?? int.MaxValue;
-        if (i >= Parent?.Packets.Count) return null;
-        return Parent?.Packets[i];
-    }
-    public bool Empty => Value == -1 && Packets.Count == 0;
-}
+obj.Sort((JsonElement l, JsonElement r) => Ordered(l, r));
+Console.WriteLine((obj.IndexOf(distress1) + 1) * (obj.IndexOf(distress2) + 1));
