78aabdc278b601a5d4c4e11dca00e02ce9ff6e29
Richard Goforth <dedac@github.com>
2022-12-14 17:29:38 -0500

    start day14

--- Program.cs
+++ Program.cs
@@ -1,46 +1,33 @@
-﻿using System.Text.Json;
-
-JsonElement BuildJson(string s) => JsonSerializer.Deserialize<JsonElement>(s);
-
-int Ordered(JsonElement l, JsonElement r)
+﻿var grid = new int[1000, 1000];
+var coords = File.ReadLines(args[0])
+                .Select(l => l.Split(" -> ")
+                    .Select(s => (int.Parse(s.Split(",").First()), int.Parse(s.Split(',').Last()))).ToArray());
+foreach (var r in coords)
 {
-    if (l.ValueKind == JsonValueKind.Number && r.ValueKind == JsonValueKind.Number)
+    for (int i = 0; i < r.Length - 1; i++)
     {
-        if (l.GetInt32() == r.GetInt32()) return 0;
-        return l.GetInt32() < r.GetInt32() ? -1 : 1;
+        if (r[i].Item1 != r[i + 1].Item1)
+        {
+            var start = Math.Min(r[i].Item1, r[i + 1].Item1);
+            var end = Math.Max(r[i].Item1, r[i + 1].Item1);
+            var y = r[i].Item2;
+            for (int x = start; x <= end; x++)
+                grid[x, y] = 1;
+        }
+        else if (r[i].Item2 != r[i + 1].Item2)
+        {
+            var start = Math.Min(r[i].Item2, r[i + 1].Item2);
+            var end = Math.Max(r[i].Item2, r[i + 1].Item2);
+            var x = r[i].Item1;
+            for (int y = start; y <= end; y++)
+                grid[x, y] = 1;
+        }
     }
-    if (l.ValueKind == JsonValueKind.Number)
-        l = BuildJson($"[{l.GetInt32()}]");
-    if (r.ValueKind == JsonValueKind.Number)
-        r = BuildJson($"[{r.GetInt32()}]");
-
-    var lA = l.EnumerateArray();
-    var rA = r.EnumerateArray();
-    for (int i = 0; i < Math.Min(l.GetArrayLength(), r.GetArrayLength()); i++)
-    {
-        lA.MoveNext();
-        rA.MoveNext();
-        var o = Ordered(lA.Current, rA.Current);
-        if (o != 0) return o;
-    }
-    if (l.GetArrayLength() == r.GetArrayLength()) return 0;
-    return l.GetArrayLength() < r.GetArrayLength() ? -1 : 1;
 }
 
-var obj = File.ReadLines(args[0]).SplitBy(s => s == "")
-    .SelectMany(a => a.Select(b => BuildJson(b))).ToList();
-
-var sumJ = 0;
-for (int i = 0; i < obj.Count / 2; i++)
-    if (Ordered(obj[2 * i], obj[2 * i + 1]) == -1)
-        sumJ += i + 1;
-Console.WriteLine(sumJ);
-
-var distress1 = BuildJson("[[2]]");
-var distress2 = BuildJson("[[6]]");
-
-obj.Add(distress1);
-obj.Add(distress2);
-
-obj.Sort((JsonElement l, JsonElement r) => Ordered(l, r));
-Console.WriteLine((obj.IndexOf(distress1) + 1) * (obj.IndexOf(distress2) + 1));
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 494; j < 504; j++)
+        Console.Write(grid[j, i]);
+    Console.WriteLine();
+}
