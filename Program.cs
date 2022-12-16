039e84bd1ce2679b64546ad27575ada2a04f9d1e
Richard Goforth <dedac@github.com>
2022-12-16 09:15:03 -0500

    re-use same data structure, still doesn't work

--- Program.cs
+++ Program.cs
@@ -3,26 +3,26 @@ var cleared = new bool[size];
 var beacons = File.ReadLines(args[0])
     .Select(s => s.Split('=').Skip(1)
         .Select(p => int.Parse(new string(p.TakeWhile(c => char.IsDigit(c) || c == '-')
-        .ToArray()))).ToArray());
+        .ToArray()))).ToArray()).ToArray();
 
 int row = 0;
 int col = 0;
+var flipper = true;
 for (row = 0; row < size; row++)
 {
-    foreach (var coords in beacons)
+    for (int x = 0; x< beacons.Length; x++) 
     {
+        var coords = beacons[x];
         var distance = Math.Abs(coords[0] - coords[2]) + Math.Abs(coords[1] - coords[3]);
-        {
-            var distOnRow = distance - Math.Abs(coords[1] - row);
-            var start = Math.Max(coords[0] - distOnRow, 0);
-            var end = Math.Min(start + distOnRow * 2 + 1, size);
-            for (col = start; col < end; col++)
-                cleared[col] = true;
-        }
+        var distOnRow = distance - Math.Abs(coords[1] - row);
+        var start = Math.Max(coords[0] - distOnRow, 0);
+        var end = Math.Min(start + distOnRow * 2 + 1, size);
+        for (col = start; col < end; col++)
+            cleared[col] = flipper;
     }
-    if (!cleared.All(c=>c)) break;
-    cleared = new bool[size];
+    if (!cleared.All(c => flipper == c)) break;
+    flipper = !flipper;
 }
 var i = 0;
-while(cleared[i]) i++;
+while (cleared[i] == flipper && i<size) i++;
 Console.WriteLine(i * 4000000 + row);
