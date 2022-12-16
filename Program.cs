03cff66d902b853b6727dc7a273712b2efc67be7
Richard Goforth <dedac@github.com>
2022-12-16 14:17:41 -0500

    day15 complete

--- Program.cs
+++ Program.cs
@@ -1,28 +1,32 @@
-﻿var size = 4000001;
-var cleared = new bool[size];
+﻿var sw = System.Diagnostics.Stopwatch.StartNew();
+var size = 4000001;
 var beacons = File.ReadLines(args[0])
     .Select(s => s.Split('=').Skip(1)
         .Select(p => int.Parse(new string(p.TakeWhile(c => char.IsDigit(c) || c == '-')
         .ToArray()))).ToArray()).ToArray();
 
 int row = 0;
-int col = 0;
-var flipper = true;
+var distress = -1;
 for (row = 0; row < size; row++)
 {
-    for (int x = 0; x< beacons.Length; x++) 
+    var cleared = new List<(int,int)>();
+    for (int x = 0; x < beacons.Length; x++)
     {
         var coords = beacons[x];
-        var distance = Math.Abs(coords[0] - coords[2]) + Math.Abs(coords[1] - coords[3]);
-        var distOnRow = distance - Math.Abs(coords[1] - row);
-        var start = Math.Max(coords[0] - distOnRow, 0);
-        var end = Math.Min(start + distOnRow * 2 + 1, size);
-        for (col = start; col < end; col++)
-            cleared[col] = flipper;
+        var distOnRow = Math.Abs(coords[0] - coords[2]) + Math.Abs(coords[1] - coords[3]) - Math.Abs(coords[1] - row);
+        if (distOnRow >= 0)
+        {
+            var start = Math.Max(coords[0] - distOnRow, 0);
+            var end = Math.Min(start + distOnRow * 2, size);
+            cleared.Add((start,end));
+        }
     }
-    if (!cleared.All(c => flipper == c)) break;
-    flipper = !flipper;
+    foreach (var range in cleared)
+    {
+        if (!cleared.Any(c => range.Item1-1 >= c.Item1 && range.Item1-1 < c.Item2))
+            distress = range.Item1 - 1;
+    }
+    if (distress != -1) break;
 }
-var i = 0;
-while (cleared[i] == flipper && i<size) i++;
-Console.WriteLine(i * 4000000 + row);
+sw.Stop();
+Console.WriteLine(distress * 4000000L + row + " " + sw.Elapsed);
---
+++ day15/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Day15
+{
+    private static void Day(string[] args)
+    {
+        var size = 4000001;
+        var beacons = File.ReadLines(args[0])
+            .Select(s => s.Split('=').Skip(1)
+                .Select(p => int.Parse(new string(p.TakeWhile(c => char.IsDigit(c) || c == '-')
+                .ToArray()))).ToArray()).ToArray();
+
+        int row = 0;
+        var distress = -1;
+        for (row = 0; row < size; row++)
+        {
+            var cleared = new List<(int, int)>();
+            for (int x = 0; x < beacons.Length; x++)
+            {
+                var coords = beacons[x];
+                var distOnRow = Math.Abs(coords[0] - coords[2]) + Math.Abs(coords[1] - coords[3]) - Math.Abs(coords[1] - row);
+                if (distOnRow >= 0)
+                {
+                    var start = Math.Max(coords[0] - distOnRow, 0);
+                    var end = Math.Min(start + distOnRow * 2, size);
+                    cleared.Add((start, end));
+                }
+            }
+            foreach (var range in cleared)
+            {
+                if (!cleared.Any(c => range.Item1 - 1 >= c.Item1 && range.Item1 - 1 < c.Item2))
+                    distress = range.Item1 - 1;
+            }
+            if (distress != -1) break;
+        }
+        Console.WriteLine(distress * 4000000L + row);
+    }
+}
